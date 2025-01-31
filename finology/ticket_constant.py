4ae756e3f754bf9abe02d2c2b2c1f11319580632
Shoeb Ahmed <36133933+shoeb370@users.noreply.github.com>
2025-02-01 00:13:16 +0530

    Create ticket_constant.py
    headers update v1.1

---
+++ finology/ticket_constant.py
@@ -0,0 +1,53 @@
+
+url = ''
+main_url_headers  = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'priority': 'u=0, i',
+    'referer': 'https://www.linkedin.com/',
+    'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+}
+
+
+api_headers = {
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'accept-language': 'en-US,en;q=0.9',
+    # 'cookie': '_gcl_au=1.1.1598731680.1736352860; ASP.NET_SessionId=sg5vrrhqzdlkuodoghefymlp; _ga=GA1.2.1317609047.1736352861; _gid=GA1.2.1461340557.1737388308; _clck=4ci867%7C2%7Cfsq%7C0%7C1834; _clsk=27zucn%7C1737390185294%7C3%7C1%7Cu.clarity.ms%2Fcollect; _ga_FDZ59LX88Z=GS1.1.1737391099.11.1.1737391366.60.0.0',
+    'priority': 'u=1, i',
+    'referer': '',
+    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+    'x-requested-with': 'XMLHttpRequest',
+}
+
+pagination_headers = headers = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    # 'cookie': '_gcl_au=1.1.1598731680.1736352860; ASP.NET_SessionId=jhjhu3eicoimadr5bhc1h5fb; _clck=4ci867%7C2%7Cfsz%7C0%7C1834; _gid=GA1.2.566532932.1738153302; _gat_gtag_UA_136614031_6=1; _ga_FDZ59LX88Z=GS1.1.1738161188.17.1.1738161333.51.0.0; _ga=GA1.2.1317609047.1736352861; _clsk=pjduk2%7C1738161335108%7C2%7C1%7Ch.clarity.ms%2Fcollect',
+    'priority': 'u=0, i',
+    'referer': 'https://ticker.finology.in/company?page=2',
+    'sec-ch-ua': '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36',
+}
+
